c3895129e2079da3560625fe381c34df04fda182
Prakash H <44009449+prakash807@users.noreply.github.com>
2021-09-06 17:58:59 -0400

    Update hangman_atmpt.py

--- hangman_atmpt.py
+++ hangman_atmpt.py
@@ -5,15 +5,19 @@ while guesses < len(unique): # While the inputted number of guesses is invalid (
     print("Not enough guesses to guess this word.") # Print message about issue
     guesses = int(input("How many guesses?")) # Reprompt for number of guesses (until valid number is given)
 length = len(correct_word)
-guessed = "_" * length # Initialize string showing which correct letters have been guessed and which have not
+guessed_word = "_" * length # Initialize string showing which correct letters have been guessed and which have not
 while guessed != correct_word and guesses != 0: # While the full word has not yet been guessed, and there are still guesses left
     letter_guess = input("Guess a letter: ")
+    guessed_letters = []
+    if letter_guess in guessed_letters:
+        print("You already guessed this.")
+        break
     for i in range(length):
         if letter_guess == correct_word[i]:
-            guessed = guessed[0:i] + correct_word[i] + guessed[i+1:length]
+            guessed_word = guessed_word[0:i] + correct_word[i] + guessed_word[i+1:length]
     print(guessed)
     guesses -= 1
-if guessed == correct_word:
+if guessed_word == correct_word:
     print("You won!")
 else:
     print("You lost!")
